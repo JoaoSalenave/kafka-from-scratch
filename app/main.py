54bec5e5f5f86c4c90bc4e37e904eef06f95cdb2
JoaoSalenave <joaog.salenave@gmail.com>
2025-03-22 11:00:07 -0300

    feat: parse API version

--- app/main.py
+++ app/main.py
@@ -9,15 +9,32 @@ def main():
     data = conn.recv(1024)
     
     if len(data) < 12:
+        print("Not enough data received")
         conn.close()
         return
+
+    offset = 4
+    request_api_key = struct.unpack('>h', data[offset:offset + 2])[0]
+    offset += 2
+
+    request_api_version = struct.unpack('>h', data[offset:offset + 2])[0]
+    offset += 2
+
+    correlation_id = struct.unpack('>i', data[offset:offset + 4])[0]
     
-    correlation_offset = 4 + 2 + 2
-    correlation_id = struct.unpack('>i', data[correlation_offset:correlation_offset + 4])[0]
-    print(f"Extracted correlation_id: {correlation_id}")
+    print(f"Request API version: {request_api_version}, correlation_id: {correlation_id}")
     
-    response_message_size = 0 
-    response = struct.pack('>i', response_message_size) + struct.pack('>i', correlation_id)
+    if request_api_version < 0 or request_api_version > 4:
+        error_code = 35  
+    else:
+        error_code = 0  
+
+    message_size = 0  
+    response = (
+        struct.pack('>i', message_size) +
+        struct.pack('>i', correlation_id) +
+        struct.pack('>h', error_code)
+    )
     
     conn.sendall(response)
     conn.close()
