1e958f109b6d3fd30b3596c03ddef7dc6ca316f3
JoaoSalenave <joaog.salenave@gmail.com>
2025-03-23 07:46:04 -0300

    fix: APIVersion handle

--- app/main.py
+++ app/main.py
@@ -14,6 +14,7 @@ def main():
         return
 
     offset = 4
+
     _ = struct.unpack('>h', data[offset:offset + 2])[0]
     offset += 2
 
@@ -21,26 +22,27 @@ def main():
     offset += 2
 
     correlation_id = struct.unpack('>i', data[offset:offset + 4])[0]
-    
-    throttle_time_ms = 0
+
+
     error_code = 0
+
     api_keys_count = 1
     api_entry = struct.pack('>h', 18) + struct.pack('>h', 0) + struct.pack('>h', 4)
-    
+
     response_body = (
-        struct.pack('>i', throttle_time_ms) +
         struct.pack('>h', error_code) +
         struct.pack('>i', api_keys_count) +
         api_entry
     )
-    
+
+
     response_header = struct.pack('>i', correlation_id)
-    
-    message_length = 20
+
+    message_length = 16
     response_message_length = struct.pack('>i', message_length)
-    
+
     response = response_message_length + response_header + response_body
-    
+
     conn.sendall(response)
     conn.close()
 
