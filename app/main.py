9e8184a15b56a2794a3c5dffec854ebaf74212a4
JoaoSalenave <joaog.salenave@gmail.com>
2025-03-23 07:49:36 -0300

    fix: use encode variant

--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
 import socket
 import struct
 
+def encode_varint(value: int) -> bytes:
+    return value.to_bytes(1, byteorder='big')
+
 def main():
     server = socket.create_server(("localhost", 9092), reuse_port=True)
     conn, addr = server.accept()
     print(f"Connected by {addr}")
-
+    
     data = conn.recv(1024)
     
     if len(data) < 12:
@@ -14,7 +17,6 @@ def main():
         return
 
     offset = 4
-
     _ = struct.unpack('>h', data[offset:offset + 2])[0]
     offset += 2
 
@@ -24,21 +26,16 @@ def main():
     correlation_id = struct.unpack('>i', data[offset:offset + 4])[0]
 
 
-    error_code = 0
-
-    api_keys_count = 1
-    api_entry = struct.pack('>h', 18) + struct.pack('>h', 0) + struct.pack('>h', 4)
-
-    response_body = (
-        struct.pack('>h', error_code) +
-        struct.pack('>i', api_keys_count) +
-        api_entry
-    )
-
-
     response_header = struct.pack('>i', correlation_id)
 
-    message_length = 16
+    error_code = struct.pack('>h', 0)
+    api_keys_array_length = encode_varint(2)
+    api_entry = struct.pack('>hhh', 18, 0, 4)
+    tagged_fields = encode_varint(0)
+    
+    response_body = error_code + api_keys_array_length + api_entry + tagged_fields
+
+    message_length = 4 + len(response_body)
     response_message_length = struct.pack('>i', message_length)
 
     response = response_message_length + response_header + response_body
