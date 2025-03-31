2d9db2160b02dd39a3ba495635e0361278479871
JoaoSalenave <joaog.salenave@gmail.com>
2025-03-31 10:45:50 -0300

    fix: general

---
+++ app/utils/constants.py
@@ -0,0 +1,30 @@
+from enum import unique, IntEnum
+from .converter import decode_int16, encode_int16
+from typing import BinaryIO
+@unique 
+class ErrorCode(IntEnum):
+    NO_ERROR = 0
+    UNKNOWN_TOPIC_OR_PARTITION = 3
+    UNSUPPORTED_VERSION = 35
+    UNKNOWN_TOPIC_ID = 100
+
+    @classmethod
+    def decode(cls, buffer: BinaryIO):
+        return ErrorCode(decode_int16(buffer))
+
+    def encode(self):
+        return encode_int16(self)
+    
+
+@unique
+class ApiKey(IntEnum):
+    API_VERSIONS = 18
+    DESCRIBE_TOPIC_PARTITIONS = 75
+    FETCH = 1
+     
+    @classmethod
+    def decode(cls, buffer: BinaryIO):
+        return ApiKey(decode_int16(buffer))
+
+    def encode(self):
+        return encode_int16(self)
